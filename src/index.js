ea37b1ccdd1058f7cbe8effdf039d4b9225f3294
Gustavo Nunes <gnunes.moura@gmail.com>
2018-05-22 13:25:32 -0300

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function asdfasdf() {
+  objetoGenerico.funcaoGenerica(
+    '*/10 * * * * *',
+    function () {
+      axios.get('http://localhost:8083/recovery/poller')
+        .then(response => {
+          console.log('[http://localhost', response.data)
+        })
+        .catch(error => {
+          console.log('[call get /recovery/poller]', error)
+        })
+    })
+}
